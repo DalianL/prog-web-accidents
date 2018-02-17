1eb1319c47150c63844d8917bc397feb8c7e5188
Matthieu Barzellino <matthieu.barzellino@etu.unice.fr>
2018-02-17 18:58:10 +0100

    Actualisation du json des accidents

--- AccidentsFilter/csvparser/src/main/java/CSVReaderExample.java
+++ AccidentsFilter/csvparser/src/main/java/CSVReaderExample.java
@@ -25,9 +25,16 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+
 import java.io.FileNotFoundException;
 import java.text.ParseException;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import org.json.simple.*;
 
 import java.io.BufferedReader;
@@ -49,20 +56,11 @@ import org.json.simple.parser.JSONParser;
 import java.util.concurrent.TimeUnit;
 import java.io.FileWriter;
 
-
-
-
-
 public class CSVReaderExample {
-
-    static String getAddressByGpsCoordinates(String lng, String lat)
-            throws IOException, org.json.simple.parser.ParseException {
-
-        URL url = new URL("http://maps.googleapis.com/maps/api/geocode/json?latlng="
-                + lat + "," + lng + "&sensor=true");
+    static String getAddressByGpsCoordinates(String lng, String lat) throws IOException, org.json.simple.parser.ParseException {
+        URL url = new URL("http://maps.googleapis.com/maps/api/geocode/json?latlng="+ lat + "," + lng + "&sensor=true");
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
         String formattedAddress = "";
-
         try {
             InputStream in = url.openStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
@@ -71,77 +69,66 @@ public class CSVReaderExample {
             while ((line = reader.readLine()) != null) {
                 result += line;
             }
-
             JSONParser parser = new JSONParser();
             JSONObject rsp = (JSONObject) parser.parse(result);
-
             if (rsp.containsKey("results")) {
                 JSONArray matches = (JSONArray) rsp.get("results");
                 JSONObject data = (JSONObject) matches.get(0); //TODO: check if idx=0 exists
                 formattedAddress = (String) data.get("formatted_address");
             }
-
             return "";
         } finally {
             urlConnection.disconnect();
             return formattedAddress;
         }
     }
-
-
-
-
-
-
-
-
-
-
-    public static void main(String[] args) throws IOException, InterruptedException,org.json.simple.parser.ParseException {
-
-        String csvFile = "/Users/mezrigui/Desktop/accidentsparser/prog-web-accidents/AccidentsFilter/csvparser/src/main/resources/Accidents.csv";
-        FileWriter file = new FileWriter("/Users/mezrigui/Desktop/accidentsparser/prog-web-accidents/AccidentsFilter/Accidents.JSON");
+    
+    public static void main(String[] args) throws IOException, InterruptedException,org.json.simple.parser.ParseException, ApiException {
+    	System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        String csvFile = "src/main/resources/Accidents.csv";
+        FileWriter file = new FileWriter("src/main/resources/Accidents.JSON");
         JSONArray tableau = new JSONArray();
         CSVReader reader = null;
+       
         try {
             reader = new CSVReader(new FileReader(csvFile));
             String[] line;
+            String region;
             int i = 0;
-
-            while (((line = reader.readNext()) != null) && ( i < 10000)) {
-                //if (((line[2].equals("")) && (line[3].equals(""))) || ((line[2].equals(null)) && (line[3].equals(null))) || ((line[2].equals("0")) && (line[3].equals("0")))) {
-
-                    System.out.println(i);
-                    Map<String, Double> coords;
-                    coords = OpenStreetMapUtils.getInstance().getCoordinates(line[1]);
-                    if ((coords.get("lat") != null) && (coords.get("lon") != null)) {
-                        //System.out.println("ladresse  " + line[1] + "   a comme atitude :" + coords.get("lat") + "et longitude" + coords.get("lon"));
-                        JSONObject obj = new JSONObject();
-
-
-
-                        obj.put("garvite", line[5]);
-                        obj.put("departement", line[4]);
-                        obj.put("lon", coords.get("lon"));
-                        obj.put("lat",coords.get("lat"));
-                        obj.put("Adresse", line[1]);
-                        obj.put("Accident-ID", line[0]);
-
-                        tableau.add(obj);
-
-
-                        //file.write(obj.toJSONString());
-                        System.out.println("Successfully Copied JSON Object to File...");
-                        System.out.println("\nJSON Object: " + obj);
-                    }
-                TimeUnit.SECONDS.sleep(1);
-                    i++;
-
+            while (((line = reader.readNext()) != null) && ( i < 10)) {
+               if(line[4].length() == 1)
+            	{
+            		region = "0" + line[4] + "000";
+            	}
+            	else
+            	{
+            		region = line[4] + "000";
+            	}
+            		
+            	GeoApiContext context = new GeoApiContext.Builder().apiKey("AIza").build();
+                	GeocodingResult[] results =  GeocodingApi.geocode(context, line[1] + " " + region).await();
+                	Gson gson = new GsonBuilder().setPrettyPrinting().create();
+  
+                	if (results.length != 0 && (gson.toJson(results[0].geometry.viewport.northeast.lng) != null) && (gson.toJson(results[0].geometry.viewport.northeast.lat) != null)) {
+                            JSONObject obj = new JSONObject();
+                            obj.put("adresse", line[1]);
+                            obj.put("departement", line[4]);
+                            obj.put("gravite", line[5]);
+                            obj.put("lon", results[0].geometry.viewport.northeast.lng);
+                            obj.put("lat", results[0].geometry.viewport.northeast.lat);
+                            obj.put("accidentId", line[0]);
+                            tableau.add(obj);
+                            file.write(obj.toJSONString());
+                            file.write(",");
+                            System.out.println("\nGoogle Object: " + obj);
+                   }
+                i++; 
             }
+            file.write(tableau.toJSONString());
+            file.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        file.write(tableau.toJSONString());
+       
     }
-
 }
